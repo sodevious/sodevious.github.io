6920526efdf9fdb253b926818c5814568091ab8b
Nicole Dominguez <sodevious.net@gmail.com>
2014-03-22 11:41:11 -0400

    Undoing testing'

--- config.rb
+++ config.rb
@@ -1,20 +1,13 @@
 require 'susy'
-# Require any additional compass plugins here.
 
-
-# Set this to the root of your project when deployed:
 css_dir = "assets/stylesheets"
 sass_dir = "assets/sass"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
-
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-
+line_comments = false
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
