0d7c2260fd71b219d6ad3e9871798776d1ccb76b
sodevious <sodevious.net@gmail.com>
2018-06-03 14:45:22 +0200

    Revert "Working on logo and header component, layout"
    This reverts commit 6a702b2f60d1f7a5127930f15cb40c0d2b396005.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,27 +1,52 @@
 <template>
-  <section class="container">
-    <div class="nd-header">
-      <logo/>
-      <header-nav/>
-    </div>
-  </section>
+  <div>
+    <nuxt/>
+  </div>
 </template>
 
-<script>
-  import Logo from '~/components/Logo.vue'
-  import HeaderNav from '~/components/HeaderNav.vue'
-
-  export default {
-    components: {
-      Logo,
-      HeaderNav
-    }
-  }
-</script>
-
 <style>
-  .nd-header {
-    display: flex;
-    justify-content: space-between;
-  }
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
 </style>
