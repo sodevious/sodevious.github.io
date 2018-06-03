6a702b2f60d1f7a5127930f15cb40c0d2b396005
sodevious <sodevious.net@gmail.com>
2018-06-03 14:43:08 +0200

    Working on logo and header component, layout

---
+++ components/HeaderNav.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="nd-nav">
+    <a href="/about" class="nd-header-cta">
+      <i class="fa fa-user"></i>
+      About
+    </a>
+
+    <a href="/assets/public/ndominguez-resume-2017.pdf" target="_blank" class="nd-header-cta">
+      <i class="fa fa-file"></i>
+      Resumé
+    </a>
+
+    <a href="/portfolio" class="nd-header-cta">
+      <i class="fa fa-image"></i>
+      Portfolio
+    </a>
+
+    <a href="/portfolio" class="nd-header-cta">
+      <i class="fa fa-image"></i>
+      Contact
+    </a>
+  </div>
+</template>
---
+++ components/Logo.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1 class="nd-logo-container">
+    <a href="/">
+      <img src="~/assets/nd-logo__purple.svg" class="nd-logo" alt="Nicole Dominguez"/>
+    </a>
+  </h1>
+</template>
+
+<style>
+  .nd-logo-container {
+    width: 25%;
+  }
+
+  .nd-logo {
+    width: 100%;
+  }
+</style>
